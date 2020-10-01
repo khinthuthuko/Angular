2b32e8e0be13da22a014b046a6fbb0b3db652554
khinthuthuko <khinthuthuko5cs51@gmail.com>
2020-10-01 16:17:58 +0630

    my angular demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './test4/test4.component';
+import { Test5Component } from './test5/test5.component';
+import { Test6Component } from './test6/test6.component';
+import { Test7Component } from './test7/test7.component';
+import { Test8Component } from './test8/test8.component';
+import { Test9Component } from './test9/test9.component';
+import { Test10Component } from './test10/test10.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    Test4Component,
+    Test5Component,
+    Test6Component,
+    Test7Component,
+    Test8Component,
+    Test9Component,
+    Test10Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test10/test10.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Test10Component } from './test10.component';
+
+describe('Test10Component', () => {
+  let component: Test10Component;
+  let fixture: ComponentFixture<Test10Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Test10Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Test10Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
